f49e835dc0e1773eecebf8f8303fba380eb4be8f
smartines <stefano.martines@sequoiasoft.com>
2023-03-24 14:43:17 +0100

    site redirect

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,17 @@
 import _Layout from "../components/_Layout";
 import NextNprogress from "nextjs-progressbar";
-import type { AppProps } from "next/app";
 import { Toaster } from "react-hot-toast";
+import { useEffect } from "react";
 import "../styles/globals.css";
 import "../styles/app.css";
 import "../styles/particles.css";
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    window.location.href = "https://the-round-table.io/";
+  }, []);
+
   return (
     <_Layout>
       <Toaster position="bottom-right" reverseOrder={false} />
